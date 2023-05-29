a909986e751e630257650e8932f3e4876de835f4
Deba Durllava Dehury <3d@Debas-MacBook-Air.local>
2023-05-29 11:11:54 +0530

    initial commit

---
+++ src/components/Editor.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+
+export default function Editor(props) {
+    const {language, displayName, value, onChange} = props
+    const [open, setOpen] = useState(true);
+    const handleChange = (editor,data, value) =>{
+        onChange(value)
+    }
+  return (
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className='editor-title'>
+        {displayName}
+        <button
+        onClick={() => setOpen(prevOpen => !prevOpen)}
+        >{open ? <CloseFullscreenIcon/> : <OpenInFullIcon/>  }</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className='code-mirror-wrapper'
+        options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            lineNumbers: true,
+            theme:'material'
+        }}
+      />
+    </div>
+    
+  )
+}
